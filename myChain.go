df58aebbf8d674d0be8720c93a270723139807db
somjetsomjet <somjet_rattanyupong@hotmail.com>
2017-04-30 21:55:43 +0700

    Update myChain.go

--- myChain.go
+++ myChain.go
@@ -31,6 +31,8 @@ func (t *MyChainCode) Query(stub shim.ChaincodeStubInterface, function string, a
 
 	Avalbytes, err := stub.GetState(args[0])
 	
+	if(err != nil )
+		return nil, err
 	jsonResp := "{\"Key\":\"" + args[0] + "\",\"Value\":\"" + string(Avalbytes) + "\"}"
 	fmt.Printf("Query Response:%s\n", jsonResp)
 	return Avalbytes, nil
